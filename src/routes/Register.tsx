9ff75190bd0aa5344305281fcf3c452065a10d9b
Zadorin Artyom <noardour@gmail.com>
2023-12-14 18:49:47 +0500

    create register route component

---
+++ src/routes/Register.tsx
@@ -0,0 +1,25 @@
+import { Button, FormControl, Link as MUILink, TextField, Typography } from '@mui/material';
+import { Form, Link } from 'react-router-dom';
+
+const Register = () => (
+  <>
+    <Typography variant="h4" sx={{ alignSelf: 'center', fontWeight: 'bold', mb: '57px' }}>
+      Регистрация
+    </Typography>
+    <FormControl component={Form} method="post" action="/login" sx={{ mb: '33px' }}>
+      <TextField label="Email*" placeholder="mail@mail.com" sx={{ mb: '8px' }} />
+      <TextField label="Как вас зовут?*" placeholder="имя фамилия" sx={{ mb: '8px' }} />
+      <TextField label="Придумайте пароль*" placeholder="введите пароль" />
+      <Typography sx={{ alignSelf: 'flex-end', mb: '46px' }}>Забыли пароль?</Typography>
+      <Button>Зарегистрироваться</Button>
+    </FormControl>
+    <Typography alignSelf="center">
+      Уже зарегестрированны?
+      <MUILink component={Link} to="/login">
+        Войти
+      </MUILink>
+    </Typography>
+  </>
+);
+
+export default Register;
