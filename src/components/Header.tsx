5e9ee9b2a8fb218c8838fb601598650f56948b75
Zadorin Artyom <noardour@gmail.com>
2023-12-18 07:20:13 +0500

    add nav animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,33 @@
 import { AppBar, Button, Toolbar, Box, FormControl, AppBarProps } from '@mui/material';
 import Logo from './Logo';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useLocation } from 'react-router-dom';
 import { FC } from 'react';
 
 interface HeaderProps extends AppBarProps {}
 
-const Header: FC<HeaderProps> = ({ sx, ...props }) => (
-  <AppBar position="static" color="secondary" sx={{ paddingTop: '21px', paddingBottom: '21px', ...sx }} {...props}>
-    <Toolbar sx={{ gap: '20px' }}>
-      <Box sx={{ flex: 1 }}>
-        <Logo />
-      </Box>
-      <Button component={Link} to="/" variant="text" color="inherit">
-        Карта
-      </Button>
-      <Button component={Link} to="/profile" variant="text" color="inherit">
-        Профиль
-      </Button>
-      <FormControl component={Form} method="post" action="/logout">
-        <Button variant="text" color="inherit" type="submit">
-          Выйти
+const Header: FC<HeaderProps> = ({ sx, ...props }) => {
+  const { pathname } = useLocation();
+
+  return (
+    <AppBar position="static" color="secondary" sx={{ paddingTop: '21px', paddingBottom: '21px', ...sx }} {...props}>
+      <Toolbar sx={{ gap: '20px' }}>
+        <Box sx={{ flex: 1 }}>
+          <Logo />
+        </Box>
+        <Button component={Link} to="/" variant="text" color={pathname === '/' ? 'primary' : 'inherit'}>
+          Карта
         </Button>
-      </FormControl>
-    </Toolbar>
-  </AppBar>
-);
+        <Button component={Link} to="/profile" variant="text" color={pathname === '/profile' ? 'primary' : 'inherit'}>
+          Профиль
+        </Button>
+        <FormControl component={Form} method="post" action="/logout">
+          <Button variant="text" color="inherit" type="submit">
+            Выйти
+          </Button>
+        </FormControl>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
